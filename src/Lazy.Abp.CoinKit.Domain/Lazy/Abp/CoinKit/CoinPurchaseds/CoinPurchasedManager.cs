50e44dafedcbe983a82ec831f59253b412e33d58
Scriptfan <scriptfan@hotmail.com>
2021-05-26 23:19:54 +0800

    编写硬币操作功能

--- src/Lazy.Abp.CoinKit.Domain/Lazy/Abp/CoinKit/CoinPurchaseds/CoinPurchasedManager.cs
+++ src/Lazy.Abp.CoinKit.Domain/Lazy/Abp/CoinKit/CoinPurchaseds/CoinPurchasedManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lazy.Abp.CoinKit.Coins;
+using System;
 using System.Threading.Tasks;
 using Volo.Abp.Domain.Services;
 using Volo.Abp.Uow;
@@ -8,10 +9,21 @@ namespace Lazy.Abp.CoinKit.CoinPurchaseds
     public class CoinPurchasedManager : DomainService, IUnitOfWorkEnabled
     {
         private readonly ICoinPurchasedRepository _repository;
+        private readonly ICoinProductRepository _productRepository;
+        private readonly ICoinWalletRepository _walletRepository;
+        private readonly ICoinWalletLogRepository _walletLogRepository;
 
-        public CoinPurchasedManager(ICoinPurchasedRepository repository)
+        public CoinPurchasedManager(
+            ICoinPurchasedRepository repository,
+            ICoinProductRepository productRepository,
+            ICoinWalletRepository walletRepository,
+            ICoinWalletLogRepository walletLogRepository
+        )
         {
             _repository = repository;
+            _productRepository = productRepository;
+            _walletRepository = walletRepository;
+            _walletLogRepository = walletLogRepository;
         }
 
         public async Task<CoinPurchased> GetAsync(Guid id)
@@ -26,21 +38,97 @@ namespace Lazy.Abp.CoinKit.CoinPurchaseds
                 return await _repository.GetAsync(id);
             }
         }
-                
+        
+        /// <summary>
+        /// 充值
+        /// </summary>
         [UnitOfWork]
-        public async Task<CoinPurchased> CreateAsync(
+        public async Task<CoinPurchased> RechargeAsync(
             Guid? tenantId,
             Guid userId,
             Guid coinProductId,
-            int quantity,
+            int productQuantity,
             string orderId,
-            int coinCount,
-            decimal paidAmount
+            int coinTotal,
+            decimal paidAmount,
+            string title,
+            string description
         )
         {
-            var coinPurchases = new CoinPurchased(GuidGenerator.Create(), tenantId, userId, coinProductId, quantity, orderId, coinCount, paidAmount);
+            using (CurrentTenant.Change(tenantId))
+            {
+                var coinPurchased = new CoinPurchased(GuidGenerator.Create(), tenantId, userId, coinProductId, productQuantity, orderId, coinTotal, paidAmount);
 
-            return await _repository.InsertAsync(coinPurchases);
+                await _repository.InsertAsync(coinPurchased);
+
+                // 修改销售数量
+                var product = await _productRepository.GetAsync(coinProductId);
+                product.SetSoldQuantity(product.SoldQuantity + productQuantity);
+
+                // 钱包
+                var wallet = await _walletRepository.GetByUserIdAsync(userId);
+                wallet.IncBalance(coinTotal);
+
+                // 写日志
+                var log = new CoinWalletLog(GuidGenerator.Create(), tenantId, userId, "Recharge", false, coinTotal, wallet.Balance, title, description);
+                await _walletLogRepository.InsertAsync(log);
+
+                return coinPurchased;
+            }
+        }
+
+        /// <summary>
+        /// 支付
+        /// </summary>
+        [UnitOfWork]
+        public async Task<int> PayAsync(
+            Guid? tenantId,
+            Guid userId,
+            int coinTotal,
+            string title,
+            string description,
+            string typeName = "Pay"
+        )
+        {
+            using (CurrentTenant.Change(tenantId))
+            {
+                // 钱包
+                var wallet = await _walletRepository.GetByUserIdAsync(userId);
+                wallet.DecBalance(coinTotal);
+
+                // 写日志
+                var log = new CoinWalletLog(GuidGenerator.Create(), tenantId, userId, typeName, true, coinTotal, wallet.Balance, title, description);
+                await _walletLogRepository.InsertAsync(log);
+
+                return wallet.Balance;
+            }
+        }
+
+        /// <summary>
+        /// 存（赚取，赠送等得到的消费点）
+        /// </summary>
+        [UnitOfWork]
+        public async Task<int> DepositAsync(
+            Guid? tenantId,
+            Guid userId,
+            int coinTotal,
+            string title,
+            string description,
+            string typeName = "Deposit"
+        )
+        {
+            using (CurrentTenant.Change(tenantId))
+            {
+                // 钱包
+                var wallet = await _walletRepository.GetByUserIdAsync(userId);
+                wallet.IncBalance(coinTotal);
+
+                // 写日志
+                var log = new CoinWalletLog(GuidGenerator.Create(), tenantId, userId, typeName, false, coinTotal, wallet.Balance, title, description);
+                await _walletLogRepository.InsertAsync(log);
+
+                return wallet.Balance;
+            }
         }
     }
 }
